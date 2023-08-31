33e9f130ccccdbaf09a21ebc88f14a7bdf528840
mohammmedsinan <mohammedbusiness1099@gmail.com>
2023-08-31 09:22:00 +0300

    Technologies page

--- src/data.js
+++ src/data.js
@@ -1,35 +1,81 @@
-export const data = {
-data: {
-    SEO:{
-        
-    },
+export let data = {
+  data: {
+    SEO: {},
     colorPallet: {
-        PrimaryColor: "#220a4e",
-        secondaryColor: "#fb6a52",
-        Accent: "#FFFFFF"
+      PrimaryColor: "#040D12",
+      secondaryColor: "#93B1A6",
+      Accent: "#FFFFFF",
     },
     Navigation: [
-        {id:1, name:"Home",link:"/"},
-        {id:2, name:"Work",link:"/portfolio"},
-        {id:3, name:"Skills",link:"/technologies"},
-        {id:4, name:"Blog",link:"/blog"},
-    ]
-},
+      { id: 1, name: "Home", link: "/" },
+      { id: 2, name: "Work", link: "/portfolio" },
+      { id: 3, name: "Skills", link: "/technologies" },
+      { id: 4, name: "Blog", link: "/blog" },
+    ],
+  },
 
-home: {
+  home: {
     title: "Mohammed Sinan || Portfolio & Resume",
     header: "Mohammed Sinan",
     plot: "Welcome to my portfolio! I'm a front-end developer with a passion for creating beautiful and intuitive web experiences. With 3 years of experience in the industry, I specialize in developing responsive, accessible, and user-friendly websites and web applications. My journey into web development began when I discovered my love for design and technology. I found that I could combine my creative skills with my technical abilities to bring digital products to life. Since then, I have been honing my skills in HTML, CSS, and JavaScript, as well as front-end frameworks like React and NextJS.",
-    plotV2: "Feel free to contact me on these platforms, Also you can check out my GitHub repositories, All of my projects have been published on GitHub.",
-    Icons: [{name:"radix-icons:github-logo",link:"https://github.com/mohammmedsinan?tab=repositories"},{name:"entypo-social:linkedin-with-circle",link:"https://www.linkedin.com/in/mohammed-sinan-396640260/"},{name:"mdi:twitter-circle",link:"https://twitter.com/MohammedSMofeed"}],
+    plotV2:
+      "Feel free to contact me on these platforms, Also you can check out my GitHub repositories, All of my projects have been published on GitHub.",
+    Icons: [
+      {
+        name: "radix-icons:github-logo",
+        link: "https://github.com/mohammmedsinan?tab=repositories",
+      },
+      {
+        name: "entypo-social:linkedin-with-circle",
+        link: "https://www.linkedin.com/in/mohammed-sinan-396640260/",
+      },
+      {
+        name: "mdi:twitter-circle",
+        link: "https://twitter.com/MohammedSMofeed",
+      },
+    ],
     resume: "./Mohammed_Sinan_CV.pdf",
-    contact:"mailto:mohammedbusiness1099@gmail.com"
-},
-skill: {
-    icons:[{icon:"",name:""},{icon:"",name:""},{icon:"",name:""},{icon:"",name:""},{icon:"",name:""},{icon:"",name:""},{icon:"",name:""},{icon:"",name:""},{icon:"",name:""},{icon:"",name:""},{icon:"",name:""},]
-
-},
-blog: {
-    title:"Blog"
-}
-}
+    contact: "mailto:mohammedbusiness1099@gmail.com",
+  },
+    portfolio: {
+        work: [
+            {name:"Dashboard",image:"",github:'https://github.com/mohammmedsinan/Sulation-For-Redux-Complexity-/tree/polished',live:"https://dashboard-react-2.netlify.app/"},
+            {name:"Integrator",image:"",github:'https://github.com/mohammmedsinan/Integrator',live:"https://integrator-project.netlify.app/"},
+            {name:"Portfolio",image:"",github:'https://github.com/mohammmedsinan/Re-Build-Website',live:"https://majestic-jalebi-06c8e2.netlify.app/"},
+            {name:"Antd",image:"",github:'https://github.com/mohammmedsinan/SRC-Code-ANTD-',live:"https://monumental-boba-49323c.netlify.app/"},
+            {name:"Greener",image:"",github:'https://github.com/mohammmedsinan/Re-Build-Website',live:"https://euphonious-zuccutto-7e305e.netlify.app/"},
+            {name:"Purple",image:"",github:'https://github.com/mohammmedsinan/Re-Build-Website-2',live:"https://wonderful-creponne-1c1595.netlify.app/"},
+        ]
+  },
+  skill: {
+    icons: [
+      { icon: "mdi:language-html5", name: "HTML" },
+      { icon: "mdi:language-css3", name: "CSS" },
+      { icon: "mdi:language-javascript", name: "Javascript" },
+      { icon: "mdi:language-typescript", name: "Typescript" },
+      { icon: "bx:bxl-less", name: "LESS" },
+      { icon: "mdi:sass", name: "SCSS" },
+      { icon: "mdi:tailwind", name: "Tailwind" },
+      { icon: "mdi:react", name: "React" },
+      { icon: "akar-icons:redux-fill", name: "Redux" },
+      { icon: "akar-icons:nextjs-fill", name: "NextJs" },
+      { icon: "mdi:api", name: "API Rest" },
+      { icon: "simple-icons:antdesign", name: "ANTd" },
+      { icon: "simple-icons:bootstrap", name: "Bootstrap" },
+      { icon: "simple-icons:graphql", name: "GraphiQL" },
+      { icon: "simple-icons:wordpress", name: "Wordpress" },
+      { icon: "simple-icons:firebase", name: "Firebase" },
+      { icon: "simple-icons:astro", name: "Astro" },
+      { icon: "simple-icons:gatsby", name: "Gatsby" },
+      { icon: "simple-icons:express", name: "Express" },
+      { icon: "mdi:git", name: "Git" },
+      { icon: "simple-icons:auth0", name: "Oauth2" },
+      { icon: "simple-icons:mongodb", name: "MongoDB" },
+      { icon: "simple-icons:chartdotjs", name: "ChartJS" },
+      { icon: "simple-icons:webcomponentsdotorg", name: "Component" },
+    ],
+  },
+  blog: {
+    title: "Blog",
+  },
+};
