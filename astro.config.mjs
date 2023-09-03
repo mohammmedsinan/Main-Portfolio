99a55dd84a3ce45822e3b5e5a61456011fe0ba0a
mohammmedsinan <mohammedbusiness1099@gmail.com>
2023-09-03 23:27:06 +0300

    trigger

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,11 @@ import image from '@astrojs/image';
 
 
 export default defineConfig({
+   vite: {
+    ssr: {
+      external: ['svgo']
+    }
+  },
   experimental: {
     viewTransitions: true
    },
