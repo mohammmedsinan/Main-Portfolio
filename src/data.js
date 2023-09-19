d01829b4567a084ae89379c0aeb1a59a042b00f4
mohammmedsinan <mohammedbusiness1099@gmail.com>
2023-09-19 09:35:36 +0300

    titles change

--- src/data.js
+++ src/data.js
@@ -15,7 +15,7 @@ export const data = {
   },
 
   home: {
-    title: "Mohammed Sinan || Portfolio & Resume",
+    title: "Mohammed Sinan || Portfolio & Blog",
     header: "Mohammed Sinan",
     plot: "Welcome to my portfolio! I'm a front-end developer with a passion for creating beautiful and intuitive web experiences. With 3 years of experience in the industry, I specialize in developing responsive, accessible, and user-friendly websites and web applications. My journey into web development began when I discovered my love for design and technology. I found that I could combine my creative skills with my technical abilities to bring digital products to life. Since then, I have been honing my skills in HTML, CSS, and JavaScript, as well as front-end frameworks like React and NextJS.",
     plotV2:
